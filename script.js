a4fdb55720a4407ec59db7d04a312f19c6a529ca
cdlyons1296 <93620791+cdlyons1296@users.noreply.github.com>
2022-12-01 08:52:59 -0500

    Commit

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ fetch("https://low-malleable-router.glitch.me/my_degrees.json").then((response) 
     console.log(data);
     let print = "";
     for(let i = 0; i < data.length; i++){
-    print += `I received (or will recieve) a ${data[i].Type} in 
+    print += `I received a ${data[i].Type} in 
     ${data[i].Program} 
     from ${data[i].School} in 
     ${data[i].Year}. <br>`;
