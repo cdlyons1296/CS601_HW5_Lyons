f7b95dc0318bfdd033f805641d6cf2082a214298
cdlyons1296 <93620791+cdlyons1296@users.noreply.github.com>
2022-12-01 08:03:37 -0500

    Final Draft

---
+++ script.js
@@ -0,0 +1,18 @@
+function getDegrees(){
+fetch("https://low-malleable-router.glitch.me/my_degrees.json").then((response) =>{
+    console.log('resolved', response);
+    return response.json();
+}).then(data =>{
+    console.log(data);
+    let print = "";
+    for(let i = 0; i < data.length; i++){
+    print += `I received (or will recieve) a ${data[i].Type} in 
+    ${data[i].Program} 
+    from ${data[i].School} in 
+    ${data[i].Year}. <br>`;
+    }
+    document.getElementById("data").innerHTML = print;
+}).catch((err) => {
+    console.log('rejectd', err);
+});
+}
